fc2382ee4aa33d087bb45a4a947ae1f9bef8e19e
regorxxx <regorxxx@protonmail.com>
2025-12-24 16:18:38 +0100

    Bump version

--- timeline.js
+++ timeline.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
-//23/12/25
+//24/12/25
 
-if (!window.ScriptInfo.PackageId) { window.DefineScript('Timeline-SMP', { author: 'regorxxx', version: '2.4.0', features: { drag_n_drop: true, grab_focus: true } }); }
+if (!window.ScriptInfo.PackageId) { window.DefineScript('Timeline-SMP', { author: 'regorxxx', version: '2.4.0-beta', features: { drag_n_drop: true, grab_focus: true } }); }
 
 include('helpers\\helpers_xxx.js');
 /* global globTags:readable, globQuery:readable, globProfiler:readable, folders:readable, VK_CONTROL:readable, clone:readable, VK_ALT:readable, dropEffect:readable, MK_CONTROL:readable */
